74276cf5ef0e9f7544983da0ad62bf061ba6246a
gowrikandasamy <45310762+gowrikandasamy@users.noreply.github.com>
2024-09-02 22:04:56 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,6 +11,7 @@ locals {
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  ingress_cidr_blocks = ["10.10.0.0/16"] 
   tags = {
     Example = local.name
   }
